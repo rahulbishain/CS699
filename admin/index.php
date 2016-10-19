f369dc18875295c433d371c04a6f6317d94966d4
Rahul Bishain <rahul.bishain@gmail.com>
2016-10-19 17:25:33 +0530

    modified process.php in admin

--- admin/index.php
+++ admin/index.php
@@ -1,27 +1,136 @@
-
+<!DOCTYPE HTML>
 <html>
-<head>
+	<head>
 
-<title>Welcome to My Home page</title>
+    	<!-- Change author to your name -->
 
+    	<meta name="author" content="Rahul"/>
 
+        
 
+        <!-- Title of Page -->
 
-<?php
+        <title>
 
-  include_once('./classes/CMS.php');
-  $obj = new CMS();
-  $obj->host = 'localhost';
-  $obj->username = 'root';
-  $obj->password = 'root';
-  $obj->database = 'cmsdb';
-	
-  $obj->connect();
+       		LoginPage || CMS
 
-  if ( $_POST )
-    $obj->write($_POST);
+        </title>
 
-  echo ( $_GET['admin'] == 1 ) ? $obj->display_admin() : $obj->display_public();
+        
 
-?>
-</html> 
+        <!-- Bootstrap -->
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">        
+
+		<script src="../bootstrap/js/bootstrap.min.js"></script>
+
+		
+
+		<script>
+
+			<!-- Javascript goes here -->
+
+		</script>
+
+		        
+
+    </head>
+
+    
+
+    <body>
+
+    
+
+    	<!-- Page Container -->
+
+        <div class="container">
+
+        	
+
+            <div class="row">
+
+            	<div class="col-xs-12 jumbotron well well-lg">
+
+                	<strong> Login </strong> <br/>
+
+                </div>
+
+            </div>
+
+        
+
+        	<div class="row" style="min-height:500px;">
+
+            	<div class="col-xs-12">
+
+            	
+
+        		<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+
+                    <fieldset >
+
+                    <legend>Login</legend>
+
+                    <input type='hidden' name='submitted' id='submitted' value='1'/>
+
+                     
+
+                    <label for='username' >UserName*:</label>
+
+                    <input type='text' name='username' id='username'  maxlength="10" />
+
+                     
+
+                    <label for='password' >Password*:</label>
+
+                    <input type='password' name='password' id='password' maxlength="10" />
+
+                     
+
+                    <input type='submit' name='Submit' value='Submit' />
+
+                     
+
+                    </fieldset>
+
+                </form>
+
+               	</div> 
+
+         	</div>
+
+         	
+
+         	<div class="row">
+
+         		<div class="col-xs-12" style="text-align:center; background-color:black; color:white; padding:10px">
+
+                    
+
+                    <p>
+
+                        <span class="glyphicon glyphicon-copyright-mark"></span> Rahul, 2016 <br>
+
+                        All Rights Reserved.
+
+                    </p>
+
+                </div>
+
+            </div>
+
+                        
+
+       	</div>
+
+        <!-- End of page container -->
+
+                            
+
+        
+
+</body>
+</html>
--- admin/process.php
+++ admin/process.php
@@ -1,138 +1,6 @@
-<!DOCTYPE HTML>
-<html>
-	<head>
-
-    	<!-- Change author to your name -->
-
-    	<meta name="author" content="Rahul"/>
-
-        
-
-        <!-- Title of Page -->
-
-        <title>
-
-       		LoginPage || CMS
-
-        </title>
-
-        
-
-        <!-- Bootstrap -->
-
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">        
-
-		<script src="../bootstrap/js/bootstrap.min.js"></script>
-
-		
-
-		<script>
-
-			<!-- Javascript goes here -->
-
-		</script>
-
-		        
-
-    </head>
-
-    
-
-    <body>
-
-    
-
-    	<!-- Page Container -->
-
-        <div class="container">
-
-        	
-
-            <div class="row">
-
-            	<div class="col-xs-12 jumbotron well well-lg">
-
-                	<strong> Login </strong> <br/>
-
-                </div>
-
-            </div>
-
-        
-
-        	<div class="row" style="min-height:500px;">
-
-            	<div class="col-xs-12">
-
-            	
-
-        		<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
-
-                    <fieldset >
-
-                    <legend>Login</legend>
-
-                    <input type='hidden' name='submitted' id='submitted' value='1'/>
-
-                     
-
-                    <label for='username' >UserName*:</label>
-
-                    <input type='text' name='username' id='username'  maxlength="10" />
-
-                     
-
-                    <label for='password' >Password*:</label>
-
-                    <input type='password' name='password' id='password' maxlength="10" />
-
-                     
-
-                    <input type='submit' name='Submit' value='Submit' />
-
-                     
-
-                    </fieldset>
-
-                </form>
-
-               	</div> 
-
-         	</div>
-
-         	
-
-         	<div class="row">
-
-         		<div class="col-xs-12" style="text-align:center; background-color:black; color:white; padding:10px">
-
-                    
-
-                    <p>
-
-                        <span class="glyphicon glyphicon-copyright-mark"></span> Rahul, 2016 <br>
-
-                        All Rights Reserved.
-
-                    </p>
-
-                </div>
-
-            </div>
-
-                        
-
-       	</div>
-
-        <!-- End of page container -->
-
-                            
-
         
 <?php
-/*$username = trim($_POST['username']);
+$username = trim($_POST['username']);
 $password = trim($_POST['password']);
 $success = 0;
 if (($username!='eval')||($password!='eval'))
@@ -141,11 +9,9 @@ else
 {
     echo "successful";
     $success = 1;
-    header("Location: loginProcess.php"); /* Redirect browser */
-/*    exit();
+    header("Location: process.php"); /* Redirect browser */
+    exit();
 } 
 
-?>*/
+?>
 
-</body>
-</html>
